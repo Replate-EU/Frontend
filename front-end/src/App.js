b5543f7ac4b99973103f631fc6181e52aec3109e
Zak <zakadmaster@gmail.com>
2020-02-02 11:31:41 +0100

    Addition of first page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route, Link } from 'react-router-dom';
+import styled from 'styled-components';
 import './App.css';
+
+const Box = styled.div`
+display: flex;
+flex-flow: column;
+align-items: center;
+border: 3px solid black;
+padding: 20px 0px;
+margin: 10px; 
+width: 30%;
+`
+const Borders = styled.div`
+border: 3px solid black;
+padding: 10px 0;
+width: 50%;
+
+`
+const Container = styled.div`
+display: flex;
+flex-flow: column;
+justify-content: center;
+border: 3px solid black;
+height: 100vh;
+width: 50%;
+margin: -3px 0;
+`
+
+const Button = styled.button`
+background: white;
+border: 2px solid black;
+border-radius: 5px;
+padding: 25px;
+`
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Borders></Borders>
+      <Container>
+        <Switch>
+        <Route exact path="/">
+      <h1>Sign Up!</h1>
+      <div className="Boxes">
+      <Box>
+        <Link to="/business"><Button className="hover"></Button></Link>
+        <h3>I'm a business</h3>
+        <p>I want to donate food</p>
+      </Box>
+      <Box>
+        <Link to="/volunteer"><Button className="hover"></Button></Link>
+        
+        <h3>I'm a volunteer</h3>
+        <p>I want to receive donations</p>
+      </Box>
+      </div>
+      </Route>
+      </Switch>
+      </Container>
+      <Borders></Borders>
     </div>
   );
 }
