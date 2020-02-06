a17e8ef5001271a4e78e4ae8b50a190ed6d5d44c
Zak <zakadmaster@gmail.com>
2020-02-06 12:24:41 +0100

    Styling rearrangement

--- front-end/src/Components/SignUp.js
+++ front-end/src/Components/SignUp.js
@@ -1,31 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Box, Button1 } from "./styled";
+
 
 export default function SignUp() {
   return (
-    <>
+    <div className="signup">
       <h1>Sign Up!</h1>
-      <div className="Boxes">
-        <Box>
+      <div className="dives">
+        <div>
           <Link to="/business">
-            <button className="hover"></button>
+            <button className="button">I'm a business</button>
           </Link>
-          <h3>I'm a business</h3>
           <p>I want to donate food</p>
-        </Box>
-        <Box>
+        </div>
+        <div>
           <Link to="/volunteer">
-            <button className="hover"></button>
+            <button className="button">I'm a volunteer</button>
           </Link>
-
-          <h3>I'm a volunteer</h3>
           <p>I want to receive donations</p>
-        </Box>
+        </div>
+        <div>
+          <Link to="/login">
+            <button className="button">Go to login page</button>
+          </Link>
+        </div>
+        
       </div>
-      <Link to="/login">
-        <button className="button">Go to login page</button>
-      </Link>
-    </>
+      
+    </div>
   );
 }
