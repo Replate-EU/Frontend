3fd22e3cab53569822800dc4228b13c4952e765a
Zak <zakadmaster@gmail.com>
2020-02-06 20:59:13 +0100

    Signup made a bit better looking

--- front-end/src/Components/SignUp.js
+++ front-end/src/Components/SignUp.js
@@ -5,25 +5,25 @@ import { Link } from "react-router-dom";
 export default function SignUp() {
   return (
     <div className="signup">
-      <h1>Sign Up!</h1>
+      <h1>Register Here</h1>
       <div className="dives">
         <div>
           <Link to="/business">
             <button className="button">I'm a business</button>
           </Link>
-          <p>I want to donate food</p>
+          <p>I want to promote my business and minimise waste</p>
         </div>
         <div>
           <Link to="/volunteer">
             <button className="button">I'm a volunteer</button>
           </Link>
-          <p>I want to receive donations</p>
+          <p>I want to receive donations while tasting new tastes</p>
         </div>
-        <div>
+        {/* <div>
           <Link to="/login">
             <button className="button">Go to login page</button>
           </Link>
-        </div>
+        </div> */}
         
       </div>
       
