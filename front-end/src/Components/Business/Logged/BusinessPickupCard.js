26cb8d38d4a9f8685de56d0a5983fbfc1b6a598e
Zak <zakadmaster@gmail.com>
2020-02-06 10:53:11 +0100

    Discriminate between name & data (card)

--- front-end/src/Components/Business/Logged/BusinessPickupCard.js
+++ front-end/src/Components/Business/Logged/BusinessPickupCard.js
@@ -11,10 +11,9 @@ export default function BusinessPickupCard({pickup, update, delete}) {
             </div>
             <h3>Address:</h3>
             <h3>Type: {props.pickup.food_type}</h3>
-            <h3>Quantity: {props.pickup.quantity} g</h3>
+            <h3>Quantity: {props.pickup.quantity* 1000} g</h3>
             <h3>Preferred pickup time: {props.pickup.pickup_time}</h3>
              <h3>Brand: {props.pickup.business_id}</h3> {/* business_id.name */}
-             <a className="button2" onClick={addToList}>ACCEPT</a>
         </div>
     )
 }
