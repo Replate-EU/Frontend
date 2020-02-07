93b87ae1909e4fc82da4bb2ebd492894ad9e19e7
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-02-07 15:59:56 +0500

    Fancy registration

--- front-end/src/Components/SignUp.js
+++ front-end/src/Components/SignUp.js
@@ -1,32 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import volunteer from "../images/volunteer.svg";
+import business from "../images/business.svg";
 
 export default function SignUp() {
   return (
     <div className="signup">
-      <h1>Register Here</h1>
+      <h1>We're glad you're here!</h1>
       <div className="dives">
-        <div>
+        <div className="signup-card">
           <Link to="/business">
-            <button className="button-primary button-big">I'm a business</button>
+            <div className="img-wrapper">
+              <img src={business} alt="Happy little shop" className="sign-up" />
+            </div>
+            <button className="button-primary button-big">
+              I'm a business
+            </button>
           </Link>
           <p>I want to promote my business and minimise waste</p>
         </div>
-        <div>
+        <div className="signup-card">
           <Link to="/volunteer">
-            <button className="button-primary button-big">I'm a volunteer</button>
+            <div className="img-wrapper">
+              <img src={volunteer} alt="Happy volunteer" className="sign-up" />
+            </div>
+            <button className="button-primary button-big">
+              I'm a volunteer
+            </button>
           </Link>
-          <p>I want to receive donations while tasting new tastes</p>
+          <p>I want to to deliver donations to those who need them</p>
         </div>
         {/* <div>
           <Link to="/login">
             <button className="button">Go to login page</button>
           </Link>
         </div> */}
-        
       </div>
-      
     </div>
   );
 }
